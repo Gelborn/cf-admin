9171a0994b90b7730316e2e8d6ef0fc287b33cef
Gelborn <felipe-grasnievicz@hotmail.com>
2025-08-04 22:23:50 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { Layout } from './components/Layout';
 import { Login } from './pages/Login';
@@ -12,6 +15,7 @@ import { Restaurants } from './pages/Restaurants';
 import { OSCs } from './pages/OSCs';
       index: '/index.html',
     },
+  },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
